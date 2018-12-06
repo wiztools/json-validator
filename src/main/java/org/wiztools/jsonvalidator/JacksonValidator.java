3ddc86b1b52b2bc3aec3e8f6c5c3c35882207d7b
Subhash Chandran <subwiz@gmail.com>
2018-12-06 23:03:29 +0530

    Empty input was causing MismatchedInputException, which is now handled.

--- src/main/java/org/wiztools/jsonvalidator/JacksonValidator.java
+++ src/main/java/org/wiztools/jsonvalidator/JacksonValidator.java
@@ -1,6 +1,7 @@
 package org.wiztools.jsonvalidator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -12,7 +13,7 @@ public class JacksonValidator implements JsonValidator {
         try {
             Object jsonObject = mapper.readValue(input, Object.class);
             return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
-        } catch (com.fasterxml.jackson.core.JsonParseException ex) {
+        } catch (com.fasterxml.jackson.core.JsonParseException | MismatchedInputException ex) {
             throw new JsonParseException(ex);
         }
     }
